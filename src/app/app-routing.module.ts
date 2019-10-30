862b2b006ea8bacbf78a64d82078bdffd3cfbb9e
Alberto Basalo <albertobasalo@hotmail.com>
2019-10-30 11:37:13 +0100

    ng g m currencies --routing true --route currencies -m app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'currencies', loadChildren: () => import('./currencies/currencies.module').then(m => m.CurrenciesModule) },
 ];
 
 @NgModule({
---
+++ src/app/currencies/currencies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'a-begins-currencies',
+  templateUrl: './currencies.component.html',
+  styleUrls: ['./currencies.component.css']
+})
+export class CurrenciesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
