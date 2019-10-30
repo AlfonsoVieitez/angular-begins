7ba0deae43d5c8d2ae789680c568c21705664474
Alberto Basalo <albertobasalo@hotmail.com>
2019-10-30 11:53:36 +0100

    currencies presenters

--- src/app/currencies/currencies.module.ts
+++ src/app/currencies/currencies.module.ts
@@ -1,15 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CurrenciesRoutingModule } from './currencies-routing.module';
 import { CurrenciesComponent } from './currencies.component';
-
-
+import { SourceFormComponent } from './source-form/source-form.component';
+import { TargetListComponent } from './target-list/target-list.component';
 @NgModule({
-  declarations: [CurrenciesComponent],
-  imports: [
-    CommonModule,
-    CurrenciesRoutingModule
-  ]
+  declarations: [CurrenciesComponent, SourceFormComponent, TargetListComponent],
+  imports: [CommonModule, CurrenciesRoutingModule, FormsModule],
 })
-export class CurrenciesModule { }
+export class CurrenciesModule {}
