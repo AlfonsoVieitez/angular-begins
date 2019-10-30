e21fba9a13a66ea00b6c9adae0253336a120f18b
Alberto Basalo <albertobasalo@hotmail.com>
2019-10-30 13:14:57 +0100

    'pub'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'currencies', loadChildren: () => import('./currencies/currencies.module').then(m => m.CurrenciesModule) },
+  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
 ];
 
 @NgModule({
