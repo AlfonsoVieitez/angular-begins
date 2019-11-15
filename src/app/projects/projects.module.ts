7ee1a2df8d93cb6ceed29e517e50f8cbc8d2340a
Alberto Basalo <albertobasalo@hotmail.com>
2019-11-15 12:53:13 +0100

    Shared Module

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MinDirective } from './min-validator.directive';
+import { SharedModule } from '../shared/shared.module';
 import { ProjectDetailPresenterComponent } from './project-detail/project-detail-presenter/project-detail-presenter.component';
 import { ProjectDetailComponent } from './project-detail/project-detail.component';
 import { ProjectListComponent } from './project-list/project-list.component';
@@ -16,8 +16,7 @@ import { ProjectsComponent } from './projects.component';
     ProjectListComponent,
     ProjectDetailComponent,
     ProjectDetailPresenterComponent,
-    MinDirective,
   ],
-  imports: [CommonModule, ProjectsRoutingModule, FormsModule],
+  imports: [CommonModule, ProjectsRoutingModule, FormsModule, SharedModule],
 })
 export class ProjectsModule {}
